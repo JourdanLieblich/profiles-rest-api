5ca30e9059ce234cb68410a133f3212863ea16e9
JourdanLieblich <jslieblich@gmail.com>
2018-02-16 20:13:48 -0800

    Created a first view  and updated URL to see the results of a get request on that view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class HelloApiView(APIView):
+    """
+        Test APIView
+    """
+
+    def get(self, request, format=None):
+        """Resturns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Similar to traditional django view',
+            'is mapped manually to URLs',
+            'gives you the max power',
+        ]
+
+        return Response({ 'message': 'Hello', 'an_apiview': an_apiview })
