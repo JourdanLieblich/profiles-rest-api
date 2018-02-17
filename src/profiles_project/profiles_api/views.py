174f178ac173bdd446e2e14a78abc62ddca4750a
JourdanLieblich <jslieblich@gmail.com>
2018-02-17 12:05:45 -0800

    added filter to search through profiles

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import serializers
 from . import models
@@ -21,6 +22,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
 
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
 
 class HelloApiView(APIView):
     """
